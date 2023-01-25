b11e53a1383330bb062de4a018ced2e0add59015
CptnPluto <cptnpluto@gmail.com>
2023-01-25 16:14:41 +0200

    Fixed merge bugs.

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const eventRouter = express.Router();
-const eventController = require("../controllers/eventsController");
+// const express = require("express");
+// const eventRouter = express.Router();
+// const eventController = require("../controllers/EventController");
 
-eventRouter.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// eventRouter.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
-module.exports = eventRouter;
+// module.exports = eventRouter;
