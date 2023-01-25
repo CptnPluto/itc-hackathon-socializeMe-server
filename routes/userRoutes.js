69cf83293cb579c1f49d62498b75b07291f7b6c3
CptnPluto <cptnpluto@gmail.com>
2023-01-25 19:52:37 +0200

    Capitalizing.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -24,6 +24,6 @@ userRouter.post(
   userController.userSignup
 );
 
-userRouter.post("/login", loginIsUserExist, UserController.userLogin);
+userRouter.post("/login", loginIsUserExist, userController.userLogin);
 
 module.exports = userRouter;
